656d0d2b5af7bea9620f3aa6f3e9cd572a4f2e1d
Ivan <steblivec1000@gmail.com>
2024-03-19 21:06:38 +0300

    feat: added project structure + initialize localizations

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:pollen_tracker/common/gen/localization/app_localizations.dart';
 
 void main() {
   runApp(const MainApp());
@@ -10,11 +12,31 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      localizationsDelegates: [
+        AppLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      locale: Locale('ru'),
+      supportedLocales: [
+        Locale('en'), // English
+        Locale('ru'), // Russian
+      ],
       home: Scaffold(
         body: Center(
-          child: Text('Hello World!'),
+          child: HomePage(),
         ),
       ),
     );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text('${AppLocalizations.of(context).health_check})');
   }
 }

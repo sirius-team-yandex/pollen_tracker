0d78d8639d6fc6d9553c508a3568ec4b936e8a0b
kekulta <dvrussk@yandex.ru>
2024-03-23 20:31:23 +0300

    fix: fix formatting

--- lib/data/models/local/mood_record_model_isar.dart
+++ lib/data/models/local/mood_record_model_isar.dart
@@ -4,14 +4,23 @@ part 'mood_record_model_isar.g.dart';
 
 @collection
 class MoodRecordModelIsar {
-  Id id;
+  // Internal id for Isar, we DO NOT use it to query our objects.
+  Id id = Isar.autoIncrement;
+  // We should use composite index of ownerId + date instead.
+  // This is workaround baecause Isar does not support neither composite nor string keys.
+  // Composite index is just a way to find db entry it IS NOT unique.
+  // You should make sure that you deleted all of the other entries with the same index.
+  @Index(composite: [CompositeIndex('date')])
+  int ownerId;
+  DateTime date;
   @enumerated
   MoodType moodType;
   String? comment;
-  DateTime date;
   MoodRecordModelIsar({
+    required this.id,
+    required this.ownerId,
+    required this.date,
     required this.moodType,
     this.comment,
-    required this.date,
-  }) : id = int.parse(date.millisecondsSinceEpoch.toString().substring(0, 10));
+  });
 }

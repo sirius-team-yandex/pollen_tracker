1beba959d25736d1867d4f74a90207fd0bdf1526
Ivan <steblivec1000@gmail.com>
2024-03-23 15:35:12 +0300

    fix: fixes after failed analyzer

--- lib/main_test.dart
+++ lib/main_test.dart
@@ -13,7 +13,7 @@ import 'package:pollen_tracker/ui/theme/app_theme.dart';
 import 'package:pollen_tracker/ui/theme/theme.dart';
 
 void main() async {
-  print('delete this');
+  // print('delete this');
   runZonedGuarded(
     () async {
       WidgetsFlutterBinding.ensureInitialized();

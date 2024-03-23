66a7b9563e3efb4304901c28337c76c1283d6555
Ivan <steblivec1000@gmail.com>
2024-03-23 14:09:31 +0300

    fix

--- lib/main_test.dart
+++ lib/main_test.dart
@@ -13,6 +13,7 @@ import 'package:pollen_tracker/ui/theme/app_theme.dart';
 import 'package:pollen_tracker/ui/theme/theme.dart';
 
 void main() async {
+  //ttest
   runZonedGuarded(
     () async {
       WidgetsFlutterBinding.ensureInitialized();

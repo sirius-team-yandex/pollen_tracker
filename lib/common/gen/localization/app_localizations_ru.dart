e6e0862c8827926932f01fe1fab813f0b3670bce
Ivan <steblivec1000@gmail.com>
2024-03-26 10:52:18 +0300

    fix: formatting

--- lib/common/gen/localization/app_localizations_ru.dart
+++ lib/common/gen/localization/app_localizations_ru.dart
@@ -8,7 +8,8 @@ class AppLocalizationsRu extends AppLocalizations {
   String get health_check => 'Выберите сегодняшнее ощущение здоровья';
 
   @override
-  String get welcome_description_1 => 'Мы заботимся о Вашем здоровье и учитываем все аллергены, чтобы показывать свежий прогноз на ближайшие дни.';
+  String get welcome_description_1 =>
+      'Мы заботимся о Вашем здоровье и учитываем все аллергены, чтобы показывать свежий прогноз на ближайшие дни.';
 
   @override
   String get welcome_description_2 => 'Сохраняйте состояние своего здоровья для подробной статистики.';

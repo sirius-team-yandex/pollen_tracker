d3407169575599ffbb32b9fd703bbab63941a168
kekulta <dvrussk@yandex.ru>
2024-03-28 20:34:53 +0300

    fix: fix code style

--- lib/main.dart
+++ lib/main.dart
@@ -113,8 +113,7 @@ class _PollenAppWrapperState extends State<PollenAppWrapper> {
 class CitiesInherited extends InheritedWidget {
   final List<CityEntity> cities;
 
-  const CitiesInherited(
-      {super.key, required super.child, required this.cities});
+  const CitiesInherited({super.key, required super.child, required this.cities});
 
   static CitiesInherited of(BuildContext context) {
     return context.dependOnInheritedWidgetOfExactType<CitiesInherited>()!;
@@ -282,10 +281,8 @@ List<MoodRecordModelIsar> _generateMoodRecords(int ownerId) {
 Future<List<PollenModel>> _generatePollenModel(double lat, double lng) async {
   logger.i('loadin bundle...');
   final asset = await rootBundle.loadString('assets/SiriusPollen.json');
-  final pollenEntityToPollenModelMapper =
-      GetIt.I<PollenEntityToPollenModelMapper>();
-  final pollenDtoToPollenEntityMappper =
-      GetIt.I<PollenDtoToPollenEntityMappper>();
+  final pollenEntityToPollenModelMapper = GetIt.I<PollenEntityToPollenModelMapper>();
+  final pollenDtoToPollenEntityMappper = GetIt.I<PollenDtoToPollenEntityMappper>();
   final data = await json.decode(asset);
   final entities = pollenDtoToPollenEntityMappper.map(AmbeeDto.fromJson(data));
 
@@ -295,8 +292,7 @@ Future<List<PollenModel>> _generatePollenModel(double lat, double lng) async {
   for (var i = 0; i < 24 * 30; i++) {
     final currValue = i % entities.length;
     final currDate = DateTime.now().subtract(Duration(hours: i));
-    mappedEntities
-        .add(entities[currValue].copyWith(lat: lat, lng: lng, time: currDate));
+    mappedEntities.add(entities[currValue].copyWith(lat: lat, lng: lng, time: currDate));
   }
 
   final models = pollenEntityToPollenModelMapper.map(mappedEntities);
